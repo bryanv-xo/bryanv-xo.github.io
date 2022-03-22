ca609e124a8222ea4c5a8e46fdbc041deba3a18f
bryanv-xo <99779186+bryanv-xo@users.noreply.github.com>
2022-03-22 17:05:03 -0600

    Create action_page.php

---
+++ action_page.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+bienvenido: <?php echo $_POST["nombres"]; ?><br>
+apellido:<?php echo $_POST["apellidos"]; ?> <br>
+correo : <?php echo $_POST["correoelectronico"]; ?> <br>
+telefono : <?php echo $_POST["numerotelefonico"]; ?> <br>
+
+
+
+</body>
+</html>
